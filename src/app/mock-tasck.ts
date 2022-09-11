b4e1601028f259f67eac4d28c4e818a0d4917d9c
jesuskado <jesus.kado@gmail.com>
2022-09-11 17:32:21 -0300

    Base

---
+++ src/app/mock-tasck.ts
@@ -0,0 +1,34 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] =[
+    {
+        id:1,
+        text: "Terminar primer modulo",
+        day: 'Agosto 5 a las 12:00',
+        reminder:true
+    },
+    {
+        id:2,
+        text: "Terminar segundo modulo",
+        day: 'Agosto 6 a las 12:00',
+        reminder:false
+    },
+    {
+        id:3,
+        text: "Hacer compras",
+        day: 'Agosto 6 a las 15:00',
+        reminder:false
+    },
+    {
+        id:4,
+        text: "Leer informe",
+        day: 'Agosto 6 a las 18:00',
+        reminder:false
+    },
+    {
+        id:5,
+        text: "Hacer Ejercicios",
+        day: 'Agosto 7 a las 09:00',
+        reminder:false
+    },
+]
